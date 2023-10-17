fd237d84e63af9ddc56d6ca7b967915bf80fefc9
Johnny Liao <johnnyliao@pop-os.localdomain>
2023-10-17 01:34:36 -0700

    updated linter and formatter to use djlint

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "tsserver", "pyright", "htmldjango" }
+local servers = { "html", "cssls", "clangd", "tsserver", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
